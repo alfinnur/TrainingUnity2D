9883035b3f3559232132ec9659041e60e57841fa
alfinnur <36445393+alfinnur@users.noreply.github.com>
2018-02-13 21:17:44 +0700

    Initial commit

---
+++ Assets/script/TrainingBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingBehaviour : MonoBehaviour {
+    public int x;
+    // Use this for initialization
+    void Start () {
+        x += 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(x);
+        x += 1;
+	}
+}
